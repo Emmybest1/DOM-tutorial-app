37c2b72a6ca1be277499ba308b41e4434ba61bfb
Emmanuel Onah <emmajerry2016@gmail.com>
2020-08-27 13:28:04 +0300

    added a cypress click test for the backgroun image element

--- cypress/integration/examples/cypressTest1.js
+++ cypress/integration/examples/cypressTest1.js
@@ -1,7 +1,10 @@
 describe('===Testing AfricLite DOM Tutorial===', () => {
-  it('Visits the AfricLite DOM Tutorial Website and Click the Shop Now button', () => {
-    cy.visit('http://localhost:3000/');
-
+  it('visits the AfricLite DOM Tutorial Website and Click the Shop Now button', () => {
+    cy.visit('/');
     cy.get('button[role="button"]').click();
+  });
+
+  it('clicks each the image element', () => {
+    cy.get('img[alt=""]').click();
   });
 });
